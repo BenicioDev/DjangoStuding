8b8ef27a53ba7081a7ffaa4bae14d59bed5cbd79
Gabriel Benicio <115751289+BenicioDev@users.noreply.github.com>
2023-12-21 09:54:49 -0300

    Database with datas

--- MyAppMembers/migrations/0001_initial.py
+++ MyAppMembers/migrations/0001_initial.py
@@ -13,15 +13,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name="Membros",
             fields=[
-                (
-                    "id",
-                    models.BigAutoField(
-                        auto_created=True,
-                        primary_key=True,
-                        serialize=False,
-                        verbose_name="ID",
-                    ),
-                ),
+                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID",),),
                 ("firstname", models.CharField(max_length=30)),
                 ("lastname", models.CharField(max_length=30)),
             ],
